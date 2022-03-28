403c01d877e0b5f80956a04df5c553d3c2f84a36
Emeka Onuorah <emmykings88@gmail.com>
2022-03-28 17:53:17 +0000

    added something to commit

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = express();
 const port = 4000;
 
 app.listen(port, () => {
-  console.log(`Success! Application is running on port ${port}.`);
+  console.log(`Application is running on port ${port}.`);
 });
