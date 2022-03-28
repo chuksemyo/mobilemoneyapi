6e97d18acd86c3e0d581f9d3c67e16d9f492e02d
Emeka Onuorah <emmykings88@gmail.com>
2022-03-28 18:12:11 +0000

    Added some server config and cors feature

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 4000;
 
-app.listen(port, () => {
-  console.log(`Application is running on port ${port}.`);
+app.use(cors());
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+const corsConfig = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Credentials', 'true')
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS.POST,PUT')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept, Authorization')
+    next()
+}
+
+app.use(corsConfig);
+app.get('/', (req, res) => {
+    res.json({ "api": "Welcome to mobile money api"}) 
+})
+
+
+const server = app.listen(process.env.PORT || 5000, () => {
+    const port = server.address().port;
+    console.log(`Application is running on port ${port}.`);
 });
