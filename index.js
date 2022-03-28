41d1c9109310843b23a233a0bfd43914f0437b4d
Emeka Onuorah <emmykings88@gmail.com>
2022-03-28 17:39:56 +0000

    changed something

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = express();
 const port = 4000;
 
 app.listen(port, () => {
-  console.log(`Success! Your application is running on port ${port}.`);
+  console.log(`Success! Application is running on port ${port}.`);
 });
